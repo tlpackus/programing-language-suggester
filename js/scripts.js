8f98744e5065f1d8022cc8aa693c7596666d85f8
tlpackus <tlpackus@gmail.com>
2021-01-15 14:31:00 -0800

    been trying to add the highlight to the results so i put in some JavaScript and CSS but still having difficulty and to errors are popping up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const name1Input = $("input#name1").val();
-    console.log(name1Input);
+    const fromInput = $("input#from").val();
+    const nameInput = $("input#name").val();
+    const useInput = $("input#use").val();
 
-    $(".name1").text(name1Input);
+    $(".name").text(nameInput);
+    $(".from").text(fromInput);
 
     $("#guru-speaks").show();
+
+    if (use === 'Entrepreneurial') {
+      $(".lang2").addClass("highlightLang");
+    } else if (use === 'Industrial') {
+      $(".lang1").addClass("highlightLang");
+    } else if (use === 'Fun') {
+      $(".lang3").addClass("highlightLang");
+    }
   });
 });
